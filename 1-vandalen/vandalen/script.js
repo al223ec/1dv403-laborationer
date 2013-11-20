1ef68067f1ba77c7a4e7adda8939882d5691d6a0
al223ec <al223ec@student.lnu.se>
2013-11-20 15:34:37 +0100

    Jslint

--- 1-vandalen/vandalen/script.js
+++ 1-vandalen/vandalen/script.js
@@ -7,13 +7,13 @@ var makePerson = function (persArr) {
     //http://stackoverflow.com/questions/135448/how-do-i-check-to-see-if-an-object-has-a-property-in-javascript
 
     function hasProperty(obj, prop) {
-        var proto = obj.__proto__ || obj.constructor.prototype; // __proto__  references the same object as its internal [[Prototype]] (often referred to as "the prototype"), dvs kollar om propertien finns i prototypen
-        return (prop in obj) && (!(prop in proto) || proto[prop] !== obj[prop]); //The in operator returns true if the specified property is in the specified object.
+        //var proto = obj.__proto__ || obj.constructor.prototype; // __proto__  references the same object as its internal [[Prototype]] (often referred to as "the prototype"), dvs kollar om propertien finns i prototypen
+        return (prop in obj);// && (!(prop in proto) || proto[prop] !== obj[prop]); //The in operator returns true if the specified property is in the specified object.
         //return prop in obj; 
     }
     for(var i = 0; i < persArr.length; i++){
         if (!hasProperty(persArr[i], "name")) { throw new Error("Inte ett korekt objekt, saknas name property"); }
-        if (!(typeof persArr[i].name === "string")) { throw new Error("Name innehåller inte en string"); }
+        if (typeof persArr[i].name !== "string") { throw new Error("Name innehåller inte en string"); }
         if (!hasProperty(persArr[i], "age")) { throw new Error("Inte ett korekt objekt, saknas age property"); }
         if (isNaN(persArr[i].age)) { throw new Error("Age innehåller inte ett number"); }
     }
@@ -24,13 +24,12 @@ var makePerson = function (persArr) {
     used are entirely implementation dependent.
     */
 
-    var names = persArr.map(function (arg) { return arg.name; }).sort(function (a, b) { return a.localeCompare(b); }).join(', ');; //locale är inclusive åäö mao lokala språkinställingar
+    var names = persArr.map(function (arg) { return arg.name; }).sort(function (a, b) { return a.localeCompare(b); }).join(', '); //locale är inclusive åäö mao lokala språkinställingar
     var ageArr = persArr.map(function (arg) { return +arg.age; });
     var maxAge = Math.max.apply(Math, ageArr);
     var minAge = Math.min.apply(Math, ageArr);
 
     var averageAge = Math.round(ageArr.reduce(function (a, b) { return a += +b; }) / persArr.length);
-    console.log(averageAge);
 
 	return { "averageAge": averageAge, "maxAge": maxAge, "minAge": minAge, "names": names };
 };
