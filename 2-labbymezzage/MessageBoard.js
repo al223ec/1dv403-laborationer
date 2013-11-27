b410d1efb4f9d1f781ce879ab8e423cf7ef09e04
al223ec <al223ec@student.lnu.se>
2013-11-27 12:55:35 +0100

    Privacy leaks

--- 2-labbymezzage/MessageBoard.js
+++ 2-labbymezzage/MessageBoard.js
@@ -1,16 +1,11 @@
 ﻿function MessageBoard(container){
-    messages = [],
-    messageCounter = 0,
-
-    //messageDiv = document.createElement("div"),
-    //messageForm = form = document.createElement("form"),
-    //textarea = document.createElement("textarea"),
-    //inputButton = document.createElement("input"),
+    var messages = []; 
+    var messageNum = 0;
 
     this.textarea = document.createElement("textarea"),
     this.messageDiv = document.createElement("div"),
 
-    this.init = function(e)
+    this.init = function (e)
     {
         var messageForm = document.createElement("form"); 
         var inputButton = document.createElement("input"); 
@@ -28,42 +23,46 @@
         container.appendChild(this.messageDiv);
         container.appendChild(messageForm);
     };
-    
-    this.addMessage = function (text) {
-        text = text.trim();
-        if ((!text) || (0 === text.length)) {
-            return;
+
+    Object.defineProperties(this, {
+        messages: {
+            get: function () { return messages; },
+        },
+        messageNum: {
+            get: function () { return messageNum; },
+            set: function (value) { messageNum = value; },
         }
-        messages.push(new Message(text, new Date(), messageCounter));
-        this.addToSite(messages[messageCounter]);
-        messageCounter++;
-        console.log(messageCounter);
-    };
-
-    getMessages = function () {
-        return this.messages
-    };
-
-    this.addToSite = function (mess) {
-        var newMess = document.createElement("div");
-        var p = document.createElement("p");
-        newMess.setAttribute("class", "large-12 columns mess"); //newMess.className  = "large-12 columns";
-        var text = document.createTextNode(mess.text + mess.num + mess.date);
-
-
-        p.appendChild(text);
-        newMess.appendChild(p);
-
-        //var del = document.createElement("a");
-        //del.appendChild(document.createTextNode("Ta bort "));
-        //var details = document.createElement("a");
-        //details.appendChild(document.createTextNode(" Detaljer"));
-        //newMess.appendChild(del);
-        //newMess.appendChild(details);
-
-        this.messageDiv.appendChild(newMess);
-    };
+    });
 }
+
+MessageBoard.prototype.addMessage = function (text) {
+    text = text.trim();
+    if ((!text) || (0 === text.length)) {
+        return;
+    }
+    this.messages.push(new Message(text, new Date(), this.messageNum));
+    this.addToSite(this.messages[this.messages.length - 1]);
+    this.messageNum += 1; 
+    console.log(this.messages.length);
+};
+
+MessageBoard.prototype.addToSite = function (mess) {
+    var newMess = document.createElement("div");
+    var p = document.createElement("p");
+    newMess.setAttribute("class", "large-12 columns mess"); //newMess.className  = "large-12 columns";
+    var text = document.createTextNode(mess.text + mess.num + mess.date);
+    p.appendChild(text);
+    newMess.appendChild(p);
+
+    //var del = document.createElement("a");
+    //del.appendChild(document.createTextNode("Ta bort "));
+    //var details = document.createElement("a");
+    //details.appendChild(document.createTextNode(" Detaljer"));
+    //newMess.appendChild(del);
+    //newMess.appendChild(details);
+
+    this.messageDiv.appendChild(newMess);
+};
 
 window.onload = function () {
 
@@ -80,13 +79,13 @@ window.onload = function () {
     // Vi kopplar en eventhanterare till formulärets skickaknapp som kör en anonym funktion.
     start.addEventListener("click", function(e){
         e.preventDefault(); // Hindra formuläret från att skickas till servern. Vi hanterar allt på klienten.
-        //MessageBoard.init();
+
         arrMessageBoards.push(new MessageBoard(main));
         arrMessageBoards[numOfBoards].init();
         numOfBoards++;
-
+        console.log(numOfBoards);
         //for (var i = 0; i < arrMessageBoards.length; i++) {
-        //    console.log(arrMessageBoards[i]);
+        //    console.log(arrMessageBoards[i].getMessages().length);
         //}
     });
 };
